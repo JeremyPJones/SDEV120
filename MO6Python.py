3ce08d76d85baaf1f59294c5d16e343b0042582d
JeremyPJones <jjones1420@ivytech.edu>
2024-09-24 12:08:02 -0400

    MO6 Python Script
    MO6 Python Assignment, just messing around in Python and getting comfortable with their included IDE.

---
+++ MO6Python.py
@@ -0,0 +1,20 @@
+print("Hello World");
+print("I just wrote my first python program");
+print("I'm having so much fun");
+# The lines above are just text examples to make sure this file is runnning
+# I modified the lines a bit to have a bit of fun < 3
+
+print('\n''\n') #creating spacing between different parts of program
+
+
+name = input("Hello, please enter your name: ");
+print("Welcome, it is nice to meet you " + name + ".");
+
+print('\n''\n') #creating spacing between different parts of program
+
+c = "cats"; #c & d will be string variables to showcase concatenation
+d = "dogs";
+s = "It's raining " + c + " and " + d + "!"; #s is a little function
+
+print(s); #calling function to print string declared and initialized to 's'.
+
